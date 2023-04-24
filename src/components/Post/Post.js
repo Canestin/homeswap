52d1ece14e623d1b5b209191a6a857d4e2faf7f8
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-04-24 05:01:23 +0200

    chore: add post component

---
+++ src/components/Post/Post.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Post.module.css";
+import likeIcon from "../../images/like-icon.png";
+import starIcon from "../../images/star.png";
+
+export default function Post({ img, location, dates, rating }) {
+	return (
+		<div className={styles.container}>
+			<div className={`${styles.starIcon} ${styles.icon}`}>
+				<img src={starIcon} alt="star icon" /> <span>{rating}</span>
+			</div>
+			<div className={`${styles.likeIcon} ${styles.icon}`}>
+				<img src={likeIcon} alt="like icon" />
+			</div>
+			<div className={styles.imageContiner}>
+				<img src={img} alt="Post" />
+			</div>
+			<h3>{location[0] + ", " + location[1]}</h3>
+			<p>{dates[0] + " — " + dates[1]}</p>
+		</div>
+	);
+}
