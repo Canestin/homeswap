cc090555aaed8ad85a587b41acb0ef1047ff8c6b
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-04-29 23:49:37 +0200

    chore: add some routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import Home from "./pages/Home/Home";
-// import Availability from "./pages/Availability/Availability";
+import Availability from "./pages/Availability/Availability";
+import HouseDetails from "./pages/HouseDetails/HouseDetails";
+import NotFound from "./pages/NotFount/NotFount";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import HostProfile from "./pages/HostProfile/HostProfile";
 
 function App() {
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+
 	return (
-		<>
-			<Home />
-			{/* <Availability /> */}
-		</>
+		<div>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/availability" element={<Availability />} />
+				<Route path="/house-details" element={<HouseDetails />} />
+				<Route path="/host" element={<HostProfile />} />
+				<Route path="/*" element={<NotFound />} />
+			</Routes>
+		</div>
 	);
 }
 
