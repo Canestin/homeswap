dadb86a6f38570ce8ea64f8c5befc330cc4cc26e
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-05-02 23:21:16 +0200

    feat: add numbers component

---
+++ src/pages/AddNumbers/AddNumbers.js
@@ -0,0 +1,72 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import styles from "./AddNumbers.module.scss";
+import AddLayout from "../../components/AddLayout/AddLayout";
+import { BiPlus, BiMinus } from "react-icons/bi";
+
+const title = "Les détails sur le nombre de places";
+const description =
+	"Donnez les informations principales concernant votre logement.";
+
+function AddNumbers() {
+	const [travelers, setTravelers] = useState(0);
+	const [bedrooms, setBedrooms] = useState(0);
+	const [bathrooms, setBathrooms] = useState(0);
+	const [beds, setBeds] = useState(0);
+
+	return (
+		<div>
+			<AddLayout title={title} description={description}>
+				<div className={styles.item}>
+					<span>Travelers</span>
+					<div>
+						<div onClick={() => setTravelers((t) => (t = 0 ? t - 1 : 0))}>
+							<BiMinus />
+						</div>
+						<span>{travelers}</span>
+						<div onClick={() => setTravelers((t) => t + 1)}>
+							<BiPlus />
+						</div>
+					</div>
+				</div>
+				<div className={styles.item}>
+					<span>Bedrooms</span>
+					<div>
+						<div onClick={() => setBedrooms((t) => (t = 0 ? t - 1 : 0))}>
+							<BiMinus />
+						</div>
+						<span>{bedrooms}</span>
+						<div onClick={() => setBedrooms((t) => t + 1)}>
+							<BiPlus />
+						</div>
+					</div>
+				</div>
+				<div className={styles.item}>
+					<span>Beds</span>
+					<div>
+						<div onClick={() => setBeds((t) => (t = 0 ? t - 1 : 0))}>
+							<BiMinus />
+						</div>
+						<span>{beds}</span>
+						<div onClick={() => setBeds((t) => t + 1)}>
+							<BiPlus />
+						</div>
+					</div>
+				</div>
+				<div className={styles.item}>
+					<span>Bathrooms</span>
+					<div>
+						<div onClick={() => setBathrooms((t) => (t = 0 ? t - 1 : 0))}>
+							<BiMinus />
+						</div>
+						<span>{bathrooms}</span>
+						<div onClick={() => setBathrooms((t) => t + 1)}>
+							<BiPlus />
+						</div>
+					</div>
+				</div>
+			</AddLayout>
+		</div>
+	);
+}
+
+export default AddNumbers;
